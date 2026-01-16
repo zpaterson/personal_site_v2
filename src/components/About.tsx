323a9ed06910eb9159a22f8c99d1f4863ecdb715
Zaire Paterson <zzpaterson@gmail.com>
2026-01-15 20:37:07 -0500

    Fixing formatting in About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,10 @@
-import { 
-  WELCOME_TXT, 
-  ABOUT_TXT 
-} from "../constants/content";
+import { WELCOME_TXT, ABOUT_TXT } from "../constants/content";
 
 export const About = () => {
-  return(
-  <div>
-    <h1 className="text-cyan-800">{WELCOME_TXT}</h1>
-    <p className="whitespace-pre-line text-lg text-cyan-950">{ABOUT_TXT}</p>
-  </div>
+  return (
+    <div>
+      <h1 className="text-cyan-800">{WELCOME_TXT}</h1>
+      <p className="whitespace-pre-line text-lg text-cyan-950">{ABOUT_TXT}</p>
+    </div>
   );
-
-}
+};
